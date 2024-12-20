489fc2fd0d77882ee67bf5b27f0d13bdb5d7a50b
Sandra Jurberga-Šaudine <sandra.jurberga@gmail.com>
2024-12-20 19:53:02 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const nextConfig = {
             hostname: 'localhost',
             port: '8000',
           },
+          {
+            protocol: 'https',
+            hostname: 'pierakstspie'
+          }
         ],
       },
 
