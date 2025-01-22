e36f6205fec26daee760756d6f276619b1f33fec
Sandra Jurberga-Šaudine <sandra.jurberga@gmail.com>
2025-01-22 17:52:19 +0200

    update settings

--- src/app/admin/settings/settings.tsx
+++ src/app/admin/settings/settings.tsx
@@ -9,17 +9,24 @@ import {
 import "../../admin/admin.css"
 import { Switch } from "@/components/ui/switch"
 import { Button } from "@/components/ui/button";
-import { Service } from "../services/page";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { useRouter } from 'next/navigation'
-import { Message } from "../profile/page";
-import {toast} from "sonner"
+import { toast } from "sonner"
+import { useForm, SubmitHandler } from "react-hook-form"
+
+export interface settings {
+    day: number,
+    statuss: boolean,
+    from: string,
+    to: string,
+    breakfrom: string,
+    breakto: string
+}
 
 export default function Settings() {
-    const [workingdays, setWorkingdays] = useState([{ day: 1, statuss: true, from: "08:00", to: "17:00", breakfrom: "12:00", breakto: "13:00" }, { day: 2, statuss: true, from: "08:00", to: "17:00", breakfrom: "12:00", breakto: "13:00" }, { day: 3, statuss: true, from: "08:00", to: "17:00", breakfrom: "12:00", breakto: "13:00" }, { day: 4, statuss: true, from: "08:00", to: "17:00", breakfrom: "12:00", breakto: "13:00" }, { day: 5, statuss: true, from: "08:00", to: "17:00", breakfrom: "12:00", breakto: "13:00" }, { day: 6, statuss: false, from: "08:00", to: "17:00", breakfrom: "12:00", breakto: "13:00" }, { day: 7, statuss: false, from: "08:00", to: "17:00", breakfrom: "12:00", breakto: "13:00" }])
+    const [workingdays, setWorkingdays] = useState<settings[]>([])
     const router = useRouter()
-    const [message, setMessage] = useState<Message>()
     const weekdays = ["P.", "O.", "T.", "C.", "Pk.", "S.", "Sv."]
 
 
@@ -32,12 +39,13 @@ export default function Settings() {
         const headers = { 'Authorization': 'Bearer ' + token };
 
         axios.post(`${process.env.NEXT_PUBLIC_REQUEST_URL}/addsettings`, workingdays, { headers })
-            .then(response => { if (response.data.length > 0) { 
-                setMessage({ message: "Dati saglabāti veiksmīgi!", type: "success" }) 
-            toast.success("Dati saglabāti veiksmīgi!")
-            } else { 
-                    setMessage({ message: "Kļūda! Mēģini vēlreiz", type: "error" })
-                toast.error("Kļūda! Mēģini vēlreiz") } }) 
+            .then(response => {
+                if (response.data.length > 0) {
+                    toast.success("Dati saglabāti veiksmīgi!")
+                } else {
+                    toast.error("Kļūda! Mēģini vēlreiz")
+                }
+            })
             .catch(function (error) {
                 if (error.response.status == 401) {
                     return router.push('/login')
@@ -60,39 +68,39 @@ export default function Settings() {
 
     return (
         <>
-
             <div >
-
                 <h1 className="text-3xl w-full border-b-2">Uzstādījumi</h1>
                 <p>Uzstādiet darba laiku un pārtraukumus darba nedēļai!</p>
                 <Button onClick={saveweeksettings} className="mr-2">Saglabāt</Button>
 
-                <div className="flex row mt-2">
+                <div className="flex row mt-2 flex-wrap">
                     {workingdays.map((x, index) =>
-                        <Card key={x.day} className="mr-4 ">
+                        <Card key={x.day} className="mr-4 mt-4 ">
                             <CardHeader className=" flex items-center">
-                                <CardTitle>{weekdays[x.day -1]}</CardTitle>
+                                <CardTitle>{weekdays[x.day - 1]}</CardTitle>
                                 <Switch checked={x.statuss} onCheckedChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], statuss: e }))} />
                             </CardHeader>
                             <CardContent>
-                                <div className="flex row m-2">
-                                    <label htmlFor="from" className="mr-2">No:</label>
-                                    <input type="time" id="from" name="from" defaultValue={x.from} onChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], from: e.target.value }))} disabled={!x.statuss} style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} className="mb-2" />
-                                </div>
-                                <div>
-                                    <label htmlFor="to" className="mr-2">Līdz:</label>
-                                    <input type="time" id="to" name="to" defaultValue={x.to} disabled={!x.statuss} onChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], to: e.target.value }))} style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} />
-                                </div>
-                                <h3 className="mt-4">Pārtraukums</h3>
-                                <div className="flex row m-2">
-                                    <label htmlFor="from" className="mr-2" >No:</label>
-                                    <input type="time" id="from" name="from" defaultValue={x.breakfrom} disabled={!x.statuss} onChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], breakfrom: e.target.value }))} style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} className="mb-2" />
-                                </div>
-                                <div>
-                                    <label htmlFor="to" className="mr-2">Līdz:</label>
-                                    <input type="time" id="to" name="to" defaultValue={x.breakto} disabled={!x.statuss} onChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], breakto: e.target.value }))} style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} />
-                                </div>
-
+                                    <div className="flex row m-2">
+                                        <label htmlFor="from" className="mr-2">No:</label>
+                                        <input type="time" id="from" name="from" defaultValue={x.from} onChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], from: e.target.value }))} disabled={!x.statuss} style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} className="mb-2" />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="to" className="mr-2">Līdz:</label>
+                                        <input type="time" id="to" name="to" defaultValue={x.to} disabled={!x.statuss} onChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], to: e.target.value }))} style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} />
+                                    </div>
+                                    <h3 className="mt-4">Pārtraukums</h3>
+                                    <div className="flex row m-2">
+                                        <label htmlFor="from" className="mr-2" >No:</label>
+                                        <input type="time" id="from" name="from" defaultValue={x.breakfrom} 
+                                        disabled={!x.statuss}
+                                         onChange={(e) =>setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], breakfrom: e.target.value == "" ? "00:00" :  e.target.value }))} 
+                                         style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} className="mb-2" required />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="to" className="mr-2">Līdz:</label>
+                                        <input type="time" id="to" name="to" defaultValue={x.breakto} disabled={!x.statuss} onChange={(e) => setWorkingdays(workingdays => workingdays.with(index, { ...workingdays[index], breakto: e.target.value == "" ? "00:00" :  e.target.value  }))} style={{ border: "1px solid hsl(220 13% 91%)", borderRadius: "10px", padding: 4 }} required />
+                                    </div>
                             </CardContent>
                         </Card>
                     )}
